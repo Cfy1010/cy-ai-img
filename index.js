872853e25e7a78b61413435c13eaea4def00f03d
Cfy1010 <cy_one2000@yahoo.fr>
2022-12-07 23:17:35 +0100

    add html & css

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 // Body parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/openai', require('./routes/openaiRoutes'))
+// Set Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Sever started on port ${port}`))
+app.use('/openai', require('./routes/openaiRoutes'));
 
-
+app.listen(port, () => console.log(`Sever started on port ${port}`));
