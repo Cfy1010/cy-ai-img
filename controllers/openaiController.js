0c1eb185e9e32496cf48688b60457d68fdc8b879
Cfy1010 <cy_one2000@yahoo.fr>
2022-12-07 17:37:03 +0100

    add route & controller

---
+++ controllers/openaiController.js
@@ -0,0 +1,18 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const generateImage = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+    
+  }
+
+};
+
+module.exports = { generateImage };
